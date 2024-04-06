3c6f078140d61a2d9131ce0e7002e59fb9e1a5d5
Tgarg18 <tushargarg70091@gmail.com>
2024-04-06 14:38:59 +0530

    stylingSigninSignupPage

--- frontend/src/components/Signin/Signin.jsx
+++ frontend/src/components/Signin/Signin.jsx
@@ -1,10 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import left_image1 from '../../assets/leftimage1.png'
+import left_image2 from '../../assets/leftimage2.png'
+import left_image3 from '../../assets/leftimage3.png'
+import left_image4 from '../../assets/leftimage4.png'
+import left_image5 from '../../assets/leftimage5.png'
+import left_image6 from '../../assets/leftimage6.png'
+import left_image7 from '../../assets/leftimage7.png'
 import { NavLink } from 'react-router-dom'
+import PhoneInput from 'react-phone-input-2'
 import Logo from '../../assets/logo.svg'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const Signin = () => {
+
   const [signinMethod, setSigninMethod] = useState("phone")
+
+  const [showpassmobile, setShowpassmobile] = useState(true)
+  const [showpassemail, setShowpassemail] = useState(true)
+
+  const [passwordmobile, setPasswordmobile] = useState("")
+  const [passwordemail, setPasswordemail] = useState("")
+
+  const [phoneNumber, setPhoneNumber] = useState(null)
+  const [email, setEmail] = useState("")
+
+  const handleSigninMobile = () => {
+    console.log("Signin with mobile");
+    console.log(phoneNumber);
+    console.log(passwordmobile);
+  }
+
+  const handleSigninEmail = () => {
+    console.log("Signin with email");
+    console.log(email);
+    console.log(passwordemail);
+  }
+
+  const leftstyle = [
+    {
+        singerphoto: left_image1,
+        backgroundColorLeft: 'rgb(49,92,134)',
+        textColorLeft: 'rgb(60,141,218)'
+    },
+    {
+        singerphoto: left_image2,
+        backgroundColorLeft: 'rgb(100,60,106)',
+        textColorLeft: 'rgb(193,116,205)'
+    },
+    {
+        singerphoto: left_image3,
+        backgroundColorLeft: 'rgb(120,37,59)',
+        textColorLeft: 'rgb(210,65,103)'
+    },
+    {
+        singerphoto: left_image4,
+        backgroundColorLeft: 'rgb(186,96,83)',
+        textColorLeft: 'rgb(244,177,153)'
+    },
+    {
+        singerphoto: left_image5,
+        backgroundColorLeft: 'rgb(142,34,37)',
+        textColorLeft: 'rgb(255,89,94)'
+    },
+    {
+        singerphoto: left_image6,
+        backgroundColorLeft: 'rgb(81,78,77)',
+        textColorLeft: 'rgb(142,129,122)'
+    },
+    {
+        singerphoto: left_image7,
+        backgroundColorLeft: 'rgb(24,32,50)',
+        textColorLeft: 'rgb(71,88,125)'
+    }
+]
+
+const [color_number, setcolor_number] = useState(0)
+
+useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * 7)
+    setcolor_number(randomNumber)
+}, [])
+
   return (
     <div className='relative'>
       <div className='flex h-[10vh] bg-none w-full items-center justify-between px-5 z-10 absolute top-0'>
@@ -20,11 +97,11 @@ const Signin = () => {
         </div>
       </div>
       <div className='flex w-full h-full'>
-        <div className="left w-1/2 bg-[rgb(49,92,134)] min-h-screen">
+        <div className="left w-1/2 min-h-screen" style={{ backgroundColor: `${leftstyle[color_number].backgroundColorLeft}` }}>
           <div className='flex flex-col items-center justify-center h-full'>
-            <img src={left_image1} alt="" className='w-1/2 mb-5' />
+            <img src={leftstyle[color_number].singerphoto} alt="" className='w-1/2 mb-5' draggable="false" />
             <h2 className='text-4xl text-white font-semibold'>All Your Music.</h2>
-            <h2 className='text-2xl italic text-[rgb(83,155,218)] font-semibold'>Anytime, anywhere.</h2>
+            <h2 className='text-2xl italic font-semibold' style={{color:`${leftstyle[color_number].textColorLeft}`}}>Anytime, anywhere.</h2>
           </div>
         </div>
         <div className="right w-1/2 bg-[rgb(246,246,246)]">
@@ -36,25 +113,52 @@ const Signin = () => {
             <div className='flex flex-col items-start justify-center w-1/2 mt-7'>
               {(signinMethod == "phone") ?
                 <form action="" className='w-full'>
-                  <div className='relative w-full flex items-center'>
-                    <input type="number" className='bg-white text-black font-semibold py-3 pl-28 pr-3 rounded-3xl w-full' placeholder='Enter your mobile number' />
-                    <select name="" id="" className='absolute top-3 left-3'>
-                      <option value="India">India</option>
-                      <option value="India">USA</option>
-                      <option value="India">Canada</option>
-                      <option value="India">England</option>
-                      <option value="India">Australia</option>
-                    </select>
+                  <div className='relative w-full flex items-center flex-col gap-4'>
+                    <PhoneInput className='bg-white text-black font-semibold py-3 px-3 rounded-3xl' placeholder='Enter your mobile number'
+                      country={"in"}
+                      inputStyle={{ border: 'none' }}
+                      dropdownStyle={{ border: 'none', borderRadius: '10px', boxShadow: '0 0 5px 0 rgba(0,0,0,0.2)', backgroundColor: 'white', color: 'black' }}
+                      buttonStyle={{ border: 'none', borderRadius: '10px', boxShadow: '0 0 0px 0 rgba(0,0,0,0.2)', backgroundColor: 'white', color: 'black', padding: '0px 0px', margin: '0' }}
+                      value={phoneNumber}
+                      onChange={setPhoneNumber}
+                    />
+                    <div className='relative flex items-center w-full'>
+                      {(showpassmobile) ?
+                        <>
+                          <input type="password" className='bg-white text-black font-semibold py-3 pl-3 pr-10 rounded-3xl w-full' placeholder='Password' value={passwordmobile} onChange={(e) => setPasswordmobile(e.target.value)} />
+                          <VisibilityOffIcon onClick={() => setShowpassmobile(!showpassmobile)} style={{ cursor: 'pointer' }} className='absolute right-2 z-10' />
+                        </>
+                        :
+                        <>
+                          <input type="text" className='bg-white text-black font-semibold py-3 pl-3 pr-10 rounded-3xl w-full' placeholder='Password' value={passwordmobile} onChange={(e) => setPasswordmobile(e.target.value)} />
+                          <VisibilityIcon onClick={() => setShowpassmobile(!showpassmobile)} style={{ cursor: 'pointer' }} className='absolute right-2 z-10' />
+                        </>
+                      }
+                    </div>
                   </div>
-                  <button className='bg-[rgb(43,197,180)] text-white font-semibold py-2 px-5 rounded-3xl mt-5 text-xl w-full'>Continue</button>
+                  <NavLink>
+                  <button className='bg-[rgb(43,197,180)] text-white font-semibold py-2 px-5 rounded-3xl mt-5 text-xl w-full' onClick={() => handleSigninMobile()}>Continue</button>
+                  </NavLink>
                 </form>
                 :
                 <form action="" className='w-full'>
                   <div className='relative w-full flex flex-col gap-4 items-center'>
-                    <input type="text" className='bg-white text-black font-semibold py-3 pl-28 pr-3 rounded-3xl w-full' placeholder='Email Address' />
-                    <input type="password" className='bg-white text-black font-semibold py-3 pl-28 pr-3 rounded-3xl w-full' placeholder='Password' />
+                    <input type="text" className='bg-white text-black font-semibold py-3 pl-3 pr-3 rounded-3xl w-full' placeholder='Email Address' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <div className='w-full relative flex items-center'>
+                      {(showpassemail) ?
+                        <>
+                          <input type="password" className='bg-white text-black font-semibold py-3 pl-3 pr-10 rounded-3xl w-full' placeholder='Password' value={passwordemail} onChange={(e) => setPasswordemail(e.target.value)} />
+                          <VisibilityOffIcon onClick={() => setShowpassemail(!showpassemail)} className='absolute right-3 cursor-pointer' />
+                        </>
+                        :
+                        <>
+                          <input type="text" className='bg-white text-black font-semibold py-3 pl-3 pr-10 rounded-3xl w-full' placeholder='Password' value={passwordemail} onChange={(e) => setPasswordemail(e.target.value)} />
+                          <VisibilityIcon onClick={() => setShowpassemail(!showpassemail)} className='absolute right-3 cursor-pointer' />
+                        </>
+                      }
+                    </div>
                   </div>
-                  <button className='bg-[rgb(43,197,180)] text-white font-semibold py-2 px-5 rounded-3xl mt-5 text-xl w-full'>Continue</button>
+                  <button className='bg-[rgb(43,197,180)] text-white font-semibold py-2 px-5 rounded-3xl mt-5 text-xl w-full' onClick={() => handleSigninEmail()}>Continue</button>
                 </form>
               }
               <p className='w-full italic text-xs my-4'>
